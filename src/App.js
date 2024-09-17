f9418446c5831eca810881f010f0b83287134ff8
munir sebikaaka <munirsebikaaka@gmail.com>
2024-09-17 15:58:14 +0300

    feat:added the functionallity that generates a random letters from the parttern

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { useState } from "react";
 
+const lowerCase = "abcefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXWZ";
+
+function App() {
+  let [smallLetters, setSmallLetters] = useState("");
+  function genSmall() {
+    smallLetters += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    console.log(smallLetters);
+  }
+  function genBig() {
+    smallLetters += upperCase[Math.floor(Math.random() * upperCase.length)];
+    console.log(smallLetters);
+  }
+  return (
+    <div>
+      <SmallCase
+        onGenSmall={genSmall}
+        onSmallLetter={smallLetters}
+        onGenBig={genBig}
+      />
+    </div>
+  );
+}
+function SmallCase({ onGenSmall, onSmallLetter, onGenBig }) {
+  return (
+    <div>
+      <h1>{onSmallLetter}</h1>
+      <button onClick={onGenSmall}>ab</button>
+      <button onClick={onGenBig}>AB</button>
+    </div>
+  );
+}
 export default App;
